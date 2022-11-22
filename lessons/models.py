21e598fab85fe985a26276f4779666024430b4e2
Dean Whitbread <dean_whitbread@hotmail.com>
2022-11-22 19:01:34 +0000

    Create Lesson model with tests.
    - Create initial Lesson model.
    - Create tests for model that pass.

--- lessons/models.py
+++ lessons/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 import uuid
+#from datetime import timedelta
+from django.core.validators import MinValueValidator, RegexValidator
+
 
 #Student model
 class Student(AbstractBaseUser):
@@ -14,10 +17,20 @@ class Student(AbstractBaseUser):
     )
     USERNAME_FIELD='id'
 
-    
-    
-    
-    
-
-
-
+class Lesson(models.Model):
+    lesson_name = models.CharField(
+            # look into 'choices' field option during refactor
+            max_length=50,
+            validators = [RegexValidator(r'^[a-zA-Z ]+$')]
+        )
+    duration = models.PositiveSmallIntegerField(
+        default = 30,
+        validators = [
+            MinValueValidator(30, message="Minimum value of 30 minutes not set.")
+        ])
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    price = models.PositiveSmallIntegerField(
+        default = 30,
+        validators = [
+            MinValueValidator(1, message="Price field must be set to a value greater than zero.")
+        ])
