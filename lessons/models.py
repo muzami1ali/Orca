bfab565d4879587e2ddf8ccaa07aaf6219f312e4
Dean Whitbread <dean_whitbread@hotmail.com>
2022-11-23 17:52:35 +0000

    Resolve problem with migrations.

--- lessons/models.py
+++ lessons/models.py
@@ -32,23 +32,32 @@ class Student(AbstractUser):
     is_superuser = models.BooleanField(default=False)
 
 
-#class LessonRequest(AbstractBaseUser):
-#    number_of_lessons = models.IntegerField()
-
 class Lesson(models.Model):
     lesson_name = models.CharField(
-            # look into 'choices' field option during refactor
             max_length=50,
             validators = [RegexValidator(r'^[a-zA-Z ]+$')]
         )
     duration = models.PositiveSmallIntegerField(
         default = 30,
-        validators = [
-            MinValueValidator(30, message="Minimum value of 30 minutes not set.")
-        ])
+        validators = [MinValueValidator(30, message="Minimum value of 30 minutes not set.")]
+        )
     date = models.DateField(auto_now=False, auto_now_add=False)
     price = models.PositiveSmallIntegerField(
         default = 30,
-        validators = [
-            MinValueValidator(1, message="Price field must be set to a value greater than zero.")
-        ])
+        validators = [MinValueValidator(1, message="Price field must be set to a value greater than zero.")]
+        )
+
+    TERM_PERIOD_CHOICES =[
+        ("TERM1", "Term 1"),
+        ("TERM2", "Term 2"),
+        ("TERM3", "Term 3"),
+        ("TERM4", "Term 4"),
+        ("TERM5", "Term 5"),
+        ("TERM6", "Term 6")
+    ]
+    term_period = models.CharField(max_length = 6, choices = TERM_PERIOD_CHOICES, default = "TERM1")
+
+class LessonRequest(models.Model):
+    student = models.ForeignKey(Student,on_delete = models.CASCADE)
+    lesson = models.ForeignKey(Lesson,on_delete = models.CASCADE)
+    is_authorised = models.BooleanField(default = False)
