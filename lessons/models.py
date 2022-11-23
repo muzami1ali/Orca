b2669b4db2f01431bd392e64275d857c77e7d54f
Dean Whitbread <dean_whitbread@hotmail.com>
2022-11-23 21:43:23 +0000

    Help Harry resolve problems with student model.
    - Supposed to create another branch while helping Harry, but forget :(.

--- lessons/models.py
+++ lessons/models.py
@@ -10,23 +10,10 @@ from django.core.validators import MinValueValidator, RegexValidator
 
 #Student model
 class Student(AbstractUser):
-    username= models.CharField(
-        default='',
-        max_length=30,
-        unique=True,
-        validators=[RegexValidator(
-            regex=r'^@\w{3,}$',
-            message='Username must consist of @ followed by at least three alphanumericals'
-        )]
-    )
-    first_name=models.CharField(max_length=50,blank=False)
-    last_name=models.CharField(max_length=50,blank=False)
-    email=models.EmailField(unique=True, blank=False)
-    id=models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False
-    )
+    username = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
     # USERNAME_FIELD='id'
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
