3dbb44574f57e270cc24acb2494ba1ff62dd37a9
Dean Whitbread <dean_whitbread@hotmail.com>
2022-11-25 15:12:43 +0000

    Add comments to seed.py.

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -1,4 +1,7 @@
 '''Seed command to fill a database'''
+''' @author Dean Whitbread '''
+''' @version 25/11/2022 '''
+
 from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
 from faker import Faker
@@ -14,26 +17,26 @@ class Command(BaseCommand):
         Faker.seed(20)
 
     def handle(self, *args, **options):
-        self.seed_database()
-
-    def seed_database(self):
         try:
             self.seed_user_accounts()
-            print('Database seeding complete.')
-        except IntegrityError:
-            print('An error occurred. Unseed the database and then try again.')
 
+            print('\nDatabase seeding complete.')
+        except IntegrityError:
+            print('An error occurred. Unseed the database and try again.')
+
+
+    '''Methods to seed the database'''
     def seed_user_accounts(self):
         # create three accounts (student, administrator, and director)
-        self.create_new_student('John', 'Doe')
         print('Seeding John Doe student account...')
+        self.create_new_student('John', 'Doe')
 
-        # generate 99 student accounts (100 in total with John Doe)
+        # generate 100 student accounts
         counter = 0
         while counter < Command.STUDENT_COUNT:
+            counter += 1
             print(f'Seeding student accounts...{counter}',  end='\r')
             self.create_new_student(self.faker.first_name(), self.faker.last_name())
-            counter += 1
 
     def create_new_student(self, f_name, l_name):
         Student.objects.create_user(
