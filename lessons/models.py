0c6eab7d2734ac4e7092704851ec7f587174e9d5
Dean Whitbread <dean_whitbread@hotmail.com>
2022-11-25 15:32:20 +0000

    Add required validators that were previously removed in models.py

--- lessons/models.py
+++ lessons/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator, MinValueValidator
 
 
 class Student(AbstractUser):
