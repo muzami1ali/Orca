f2ab90255fd0590f2a55c9c0c72e7abcf1cbfc98
Dean Whitbread <dean_whitbread@hotmail.com>
2022-11-26 11:26:41 +0000

    Seed and unseed lesson objects.

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -5,22 +5,33 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
 from faker import Faker
-from lessons.models import Student
+from lessons.models import Student, Lesson
+from datetime import datetime
+import random
+
 
 class Command(BaseCommand):
     PASSWORD = "Password123"
     STUDENT_COUNT = 100
 
+    LESSON_NAMES = ['Piano Practice', 'Trumpet Techniques', 'Guitar Guidance']
+    TERM_PERIOD = ['TERM1', 'TERM2', 'TERM3', 'TERM4', 'TERM5', 'TERM6']
+    DURATION = [30, 60, 120]
+    LESSON_COUNT = 50
+
+
     def __init__(self):
         super().__init__()
         self.faker = Faker('en_GB')
         Faker.seed(20)
+        random.seed(10)
 
     def handle(self, *args, **options):
         try:
             self.seed_user_accounts()
+            self.seed_lessons()
 
-            print('\nDatabase seeding complete.')
+            print('Database seeding complete.')
         except IntegrityError:
             print('An error occurred. Unseed the database and try again.')
 
@@ -37,6 +48,7 @@ class Command(BaseCommand):
             counter += 1
             print(f'Seeding student accounts...{counter}',  end='\r')
             self.create_new_student(self.faker.first_name(), self.faker.last_name())
+        print('\n')
 
     def create_new_student(self, f_name, l_name):
         Student.objects.create_user(
@@ -45,3 +57,21 @@ class Command(BaseCommand):
             last_name = l_name,
             password=Command.PASSWORD
         )
+
+    def seed_lessons(self):
+        counter = 0
+        while counter < Command.LESSON_COUNT:
+            counter += 1
+            print(f'Seeding lessons...{counter}',  end='\r')
+            self.create_new_lesson()
+        print('\n')
+
+    def create_new_lesson(self):
+        self.lesson = Lesson(
+            lesson_name = Command.LESSON_NAMES[random.randrange(0, 3)],
+            duration = Command.DURATION[random.randrange(0, 2)],
+            date = self.faker.date_between_dates(date_start=datetime(2022,9,1), date_end=datetime(2023,7,21)),
+            price = 50,
+            term_period = Command.TERM_PERIOD[random.randrange(0, 5)]
+        )
+        self.lesson.save()
--- lessons/management/commands/unseed.py
+++ lessons/management/commands/unseed.py
@@ -1,6 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
-from lessons.models import Student
+from lessons.models import Student, Lesson
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Student.objects.filter(is_staff=False, is_superuser=False).delete()
+        self._delete_all_lessons()
+
+    def _delete_all_lessons(self):
+        for i in range(1, 7):
+            Lesson.objects.filter(term_period=f'TERM{i}').delete()
