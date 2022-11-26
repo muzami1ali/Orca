01784a60aa49f88c41a1512ddbd25e26bb45322a
Dean Whitbread <dean_whitbread@hotmail.com>
2022-11-26 18:10:26 +0000

    Add Lesson model to admin webpage.

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Lesson
 
 @admin.register(Student)
 class UserAdmin(admin.ModelAdmin):
    list_display=[
        'id','username','first_name','last_name','is_active','password',
    ]
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = [
+        field.name for field in Lesson._meta.get_fields()
+    ]
