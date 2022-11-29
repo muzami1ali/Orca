42ec57bd3559398f4b2b70a0e0a661ff36a7c90b
aleksandr limanovskij <aleksandr.limanovskij@kcl.ac.uk>
2022-11-29 21:20:38 +0000

    redefined test for lesson request model

--- lessons/tests/models/test_lesson_request_model.py
+++ lessons/tests/models/test_lesson_request_model.py
@@ -6,23 +6,29 @@ import uuid
 
 
 class LessonRequestModelTest(TestCase):
-    def test_main_case(self):
-        lesson = Lesson(
+    def setUp(self):
+        self.student=Student.objects.create_user(
+            username = 'john.doe@example.org',
+            first_name = 'John',
+            last_name = 'Doe',
+            password = 'Password123'
+        )
+        self.lesson = Lesson(
             lesson_name = "Piano Practice",
             duration = 30,
             date = "2022-11-22",
             price = 50
         )
-        lesson.save()
-        student=Student.objects.create_user(
-            username = 'john.doe@example.org',
-            first_name = 'John',
-            last_name = 'Doe',
-            password = 'Password123'            
-        )
-        student.save()
-        #lesson_request=LessonRequest(student = student, lesson = lesson, is_authorised = False)
-        #lesson_request.save()
+        self.lesson_request=LessonRequest(student =self.student,lesson = self.lesson, is_authorised = False)
 
-        #record = LessonRequest.objects.get(id=1)
-        #self.assertEqual(record.student.username, "@johndoe")
+
+
+    def test_lesson_request_has_valid_student(self):
+        self.assertEqual(self.lesson_request.student.username, "john.doe@example.org")
+        self.assertEqual(self.lesson_request.student.first_name, "John")
+        self.assertEqual(self.lesson_request.student.last_name, "Doe")
+
+    def test_lesson_request_has_valid_lesson(self):
+        self.assertEqual(self.lesson_request.lesson.lesson_name, "Piano Practice")
+        self.assertEqual(self.lesson_request.lesson.duration, 30)
+        self.assertEqual(self.lesson_request.lesson.date, "2022-11-22")
