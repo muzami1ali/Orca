cb0f8812a38604623d111324811460d0f87748db
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-11-30 17:38:45 +0000

    Add authentication to vary permission between staff and super user

--- lessons/admin.py
+++ lessons/admin.py
@@ -3,6 +3,14 @@ from .models import Student, LessonRequest, Lesson
 
 @admin.register(Student)
 class UserAdmin(admin.ModelAdmin):
+   def get_queryset(self, request):
+       qs=super().get_queryset(request)
+       if request.user.is_staff:
+           return qs
+       elif request.user.is_superuser:
+           return qs
+       return qs.filter(author=request.user)
+    
    list_display=[
        'id','username','first_name','last_name','is_active','password',
    ]
