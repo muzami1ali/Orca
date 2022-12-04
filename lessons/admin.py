b1b87fec1e4ff8ddcef37213a66e0f1de5b0807b
Harry Sharma <harrysharma1066@gmail.com>
2022-12-04 18:48:14 +0000

    Make it so details cannot be edited when the lesson aprroval is true

--- lessons/admin.py
+++ lessons/admin.py
@@ -13,24 +13,12 @@ class UserAdmin(admin.ModelAdmin):
        elif request.user.is_superuser:
            return qs
        return qs.filter(author=request.user)
-   def has_delete_permission(self, request,object=None):
-       return False
-     
-   def has_change_permission(self, request,object=None):
-       if request.user.is_staff==True or request.user.is_superuser==True:
-           return  True
-       return False
-   
-       
-   def has_add_permission(self, request):
-       if request.user.is_superuser==True:
-           return True
-       return False
-       
+    
     
    list_display=[
        'id','username','first_name','last_name','is_active','password','is_staff','is_superuser',
    ]
+   
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
