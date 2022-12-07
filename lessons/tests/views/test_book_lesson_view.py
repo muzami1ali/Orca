da8e0c3340227c5b583054c33a5fdfc0885b8bf9
muzami1ali <muzamilalibosan@icloud.com>
2022-12-07 20:54:16 +0000

    Fix some book_lesson_view tests

--- lessons/tests/views/test_book_lesson_view.py
+++ lessons/tests/views/test_book_lesson_view.py
@@ -6,7 +6,6 @@
 from django.test import TestCase
 from django.urls import reverse
 from lessons.models import Student, Lesson, LessonRequest
-this wasn't in main so i commented it out because it gave exceptions
 
 from lessons.tests.helpers import reverse_with_next
 from django.core.exceptions import ObjectDoesNotExist
@@ -22,12 +21,21 @@ class BookLessonViewTestCase(TestCase):
     def setUp(self):
         self.student = Student.objects.get(username='John.Doe@example.org')
         self.lesson = Lesson.objects.get(lesson_name='PIANO_PRACTICE')
-        self.url = reverse('request_lessons', kwargs={'LessonID': self.lesson.id})
+        self.url = reverse('request_lessons')
+        self.form_input = {
+            "lesson_name": "PIANO_PRACTICE",
+            "student_availability": "WED",
+            "number_of_lessons": 5,
+            "interval": 2,
+            "duration": 45,
+            "term_period": "TERM2",
+            "additional_information": "Please give me tutor, Jason Doe."
+        }
 
     ''' Test cases for the book lesson view - when a lesson gets booked. '''
 
     def test_url_is_valid(self):
-        self.assertEqual(self.url, f'/booking/request/{self.lesson.id}')
+        self.assertEqual(self.url, f'/booking/request/')
 
     def test_redirect_if_not_logged_in(self):
         redirect_url = reverse_with_next('login', self.url)
@@ -38,15 +46,15 @@ class BookLessonViewTestCase(TestCase):
     def test_can_book_lesson(self):
         self.client.login(username=self.student.username, password='Password123')
         counter_before = LessonRequest.objects.count()
-        response = self.client.post(self.url, data={'LessonID':self.lesson.id})
+        response = self.client.post(self.url, data=self.form_input)
         counter_after = LessonRequest.objects.count()
         self.assertEqual(counter_before + 1, counter_after)
 
     def test_cannot_book_the_same_lesson_twice(self):
         self.client.login(username=self.student.username, password='Password123')
         with self.assertRaises(IntegrityError):
-            response = self.client.post(self.url, data={'LessonID':self.lesson.id})
-            response = self.client.post(self.url, data={'LessonID':self.lesson.id}, follow=True)
+            response = self.client.post(self.url, data=self.form_input)
+            response = self.client.post(self.url, data=self.form_input)
             redirect_url = reverse_with_next('request_lessons', self.url)
             self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
