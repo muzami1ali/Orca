2c545da9906aa6e50d763d9c62106003fb06b5e7
Dean Whitbread <dean_whitbread@hotmail.com>
2022-12-07 22:52:24 +0000

    Refactor LessonRequest view.

--- lessons/views/LessonRequest.py
+++ lessons/views/LessonRequest.py
@@ -13,13 +13,18 @@ from lessons.views._HttpResponseConstantMsg import CANNOT_BOOK_TWICE_MSG
 @login_required
 def request_lessons(request):
     if request.method == "POST":
-        for previous_lesson in Lesson.objects.all():
-            if previous_lesson.equal_to(request):
+        for previous_lesson in LessonRequest.objects.filter(id=request.user.id):
+            if previous_lesson.lesson.equal_to(request):
                 return HttpResponseBadRequest(CANNOT_BOOK_TWICE_MSG)
+
+        # for previous_lesson in Lesson.objects.all():
+        #     if previous_lesson.lesson.equal_to(request):
+        #         return HttpResponseBadRequest(CANNOT_BOOK_TWICE_MSG)
 
         try:
             book_lesson = LessonRequestForm(request.POST)
             book_lesson = book_lesson.save()
+            LessonRequest.objects.create(student_id=request.user.id, lesson_id=book_lesson.id)
         except ValueError:
             return HttpResponseBadRequest("Cannot submit incomplete form.")
 
