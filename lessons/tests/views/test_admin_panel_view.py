4e3dc3d0e3589f64b44b67b1159a4deb6607e3e2
Dean Whitbread <dean_whitbread@hotmail.com>
2022-12-07 23:11:30 +0000

    Remove blank lines in admin panel tests.

--- lessons/tests/views/test_admin_panel_view.py
+++ lessons/tests/views/test_admin_panel_view.py
@@ -4,11 +4,11 @@ from lessons.models import LessonRequest,Invoice,BankTransfer
 class AdminPanelViewTestCase(TestCase):
        fixtures = [
        'lessons/tests/fixtures/admin_user_staff.json'
-       
+
     ]
        def setUp(self):
            self.url=reverse('admin_panel')
 
-           
-       
+
+
     
